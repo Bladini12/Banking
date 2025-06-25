67b5372881e534d9088432e1640f0d1f258f97ed
Bladini12 <82981596+Bladini12@users.noreply.github.com>
2025-06-25 14:22:52 +0100

    Update build.sh

--- mybackend/build.sh
+++ mybackend/build.sh
@@ -4,6 +4,7 @@
 set -e
 
 pip install -r requirements.txt
+python manage.py createsuperuser --no-input
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput 
