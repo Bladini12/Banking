4effe0aa34b1561830eafc23cc24a1b9136e391c
Bladini12 <82981596+Bladini12@users.noreply.github.com>
2025-06-25 11:52:02 +0100

    Update settings.py

--- mybackend/mybackend/settings.py
+++ mybackend/mybackend/settings.py
@@ -21,12 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h4=)bncnp#+64ihakx0*v8r$jyyut8p!&q=q5h1-nh+qy6m^4#'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-insecure-key')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
